9e639c3d55523ebdd44398a40067d4a24d5055d1
Priyanka Kulkarni <priyanka.kulkarni@thoughtworks.com>
2021-08-06 13:00:00 +0530

    Jaideb|Priyanka adding Test cases for controller endpoint to search a book from Google API

--- src/test/kotlin/com/thoughtworks/onboarding/bookInventory/controller/BookControllerTest.kt
+++ src/test/kotlin/com/thoughtworks/onboarding/bookInventory/controller/BookControllerTest.kt
@@ -2,6 +2,7 @@ package com.thoughtworks.onboarding.bookInventory.controller
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.thoughtworks.onboarding.bookInventory.model.Book
+import com.thoughtworks.onboarding.bookInventory.model.BookDto
 import com.thoughtworks.onboarding.bookInventory.service.BookService
 import org.bson.types.ObjectId
 import org.junit.jupiter.api.Test
@@ -13,6 +14,8 @@ import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import java.util.*
+import kotlin.collections.ArrayList
 
 @WebMvcTest(BookController::class)
 internal class BookControllerTest {
@@ -55,12 +58,42 @@ internal class BookControllerTest {
     }
 
     @Test
-    fun `should be able to delete` (){
+    fun `should be able to delete`() {
         val book = Book("4", null, null, null, 89.0, 8)
         `when`(bookService.delete("4")).thenReturn(book)
         mockMvc.perform(
             delete("/books/delete/{id}", "4").content(ObjectMapper().writeValueAsString(book))
                 .contentType(MediaType.APPLICATION_JSON)
         ).andExpect(status().isOk)
+    }
+
+    @Test
+    fun `should be able to search all books without count`() {
+        val bookDto = BookDto(null, "Java", "", 89.9)
+        val anotherBookDto = BookDto(null, "Java", "", 89.9)
+        val bookDtoList = ArrayList<BookDto>()
+
+        bookDtoList.add(bookDto)
+        bookDtoList.add(anotherBookDto)
+
+        `when`(bookService.search("Java", Optional.empty())).thenReturn(bookDtoList)
+        mockMvc.perform(
+            get("/books/search/").param("title", "Java")
+        ).andExpect(status().isOk)
+        verify(bookService, times(1)).search("Java", Optional.empty())
+    }
+
+    @Test
+    fun `should be able to search all books with count`() {
+        val bookDto = BookDto(null, "Java", "", 89.9)
+        val bookDtoList = ArrayList<BookDto>()
+
+        bookDtoList.add(bookDto)
+
+        `when`(bookService.search("Java", Optional.of(1))).thenReturn(bookDtoList)
+        mockMvc.perform(
+            get("/books/search/").param("title", "Java").param("count","1")
+        ).andExpect(status().isOk)
+        verify(bookService, times(1)).search("Java", Optional.of(1))
     }
 }
